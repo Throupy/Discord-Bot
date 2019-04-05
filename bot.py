cab2657ade489869c58ccd97f9ec47e1153c4aa9
Owen Throup <38217027+Throupy@users.noreply.github.com>
2019-04-06 00:13:56 +0100

    Improved getting channels
    using get_channel() instead of get()

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from utils.consts import Consts
 consts = Consts()
 bot = commands.Bot(command_prefix="~",
                    status=discord.Status.online,
-                   activity=discord.Game(name="Fucking bitches"))
+                   activity=discord.Game(name="Gaming"))
 
 @bot.event
 async def on_ready():
@@ -17,15 +17,13 @@ async def on_ready():
     print("Ready to go!")
     print(f"Serving: {len(bot.guilds)} servers!")
     await bot.change_presence(status=discord.Status.online,
-                              activity=discord.Game(name="Fucking bitches"))
+                              activity=discord.Game(name="Gaming"))
 
 
 @bot.event
 async def on_member_join(ctx):
     """Run when a new member joins."""
-    channel = get(bot.get_all_channels(),
-                  guild__name='Lube Lads',
-                  name='welcome')
+    channel = bot.get_channel(consts.channels['welcome'])
     await ctx.send(consts.welcomeDMMessage.format(ctx.name))
     msg = await channel.send(
         f"Welcome, {ctx.mention} If you agree with the rules please react to the message"
@@ -38,22 +36,16 @@ async def on_member_join(ctx):
 @bot.event
 async def on_member_ban(ctx, member):
     """Run when a member gets banned (probably kacper)."""
-    channel = get(bot.get_all_channels(),
-                  guild__name="Lube Lads",
-                  name="announcements")
+    channel = bot.get_channel(consts.channels['announcements'])
     await channel.send(f"{member.name} Just got banned from the server")
 
 @bot.event
 async def on_message(message):
-    channel = get(bot.get_all_channels(),
-                  guild__name="Lube Lads",
-                  name="announcements")
-    if message.channel.id == channel.id:
+    channel = bot.get_channel(consts.channels['announcements'])
+    if message.channel == channel:
         # If the message was NOT send by the bot
         if message.author != bot:
             await message.pin()
-            # Remove the crappy pin message
-            await message.channel.purge(limit=1)
 
 bot.load_extension("cogs.error_handler")
 bot.load_extension("cogs.main")
--- utils/consts.py
+++ utils/consts.py
@@ -9,3 +9,11 @@ Welcome, {} to the server.
 If you have any problems with bots or have any complains please
 contact one of the server owners or administrators.
 """
+
+    channels = {
+    'welcome':563820994188541967,
+    'announcements':431933100122046470,
+    'general':563471753242738698,
+    'cesspit':424237366723477506,
+    'computing':406586420468252673
+    }
